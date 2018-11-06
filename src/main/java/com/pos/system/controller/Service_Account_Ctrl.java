46cb743bde507f24e9385aa47f9f58789e791ccd
winetree <winetreee@gmail.com>
2018-11-06 10:14:00 +0900

    20181106 update
    - Spring Boot 2.1.0 업데이트
    - 불필요한 dependencies 삭제
    - 클래스 네이밍 조정

--- src/main/java/com/pos/system/controller/Service_Account_Ctrl.java
+++ src/main/java/com/pos/system/controller/Service_Account_Ctrl.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class Service_Controller {
+public class Service_Account_Ctrl {
 	
 	private final Service_Account_DAO service_Account_Dao;
 	private final Service_Account_Mapper mapper;
 	
-	public Service_Controller(Service_Account_DAO service_Account_Dao, Service_Account_Mapper mapper) {
+	public Service_Account_Ctrl(Service_Account_DAO service_Account_Dao, Service_Account_Mapper mapper) {
 		this.service_Account_Dao = service_Account_Dao;
 		this.mapper = mapper;
 	}
 	
 	@RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.GET})
 	public String home() {
+		
+		System.out.println(mapper.selectAll());
+		System.out.println(service_Account_Dao.selectAll());
+		
 		return "index";
 	}
 
