8a6faf321170469ba77b2863c78ed0909ae0bc77
Brian Park <youstarkio@gmail.com>
2018-11-07 15:14:06 +0900

    20181107 CRUD 수정 - 박성현
    - Mapper 클래스를 CRUD양식으로 변경
    - Service_Account_Create_Mapper : 회원가입
    - Service_Account_Read_Mapper : 전체 회원 조회, 상세정보 조회, 아이디 중복확인, 로그인
    - Service_Account_Update_Mapper : 회원 정보 수정
    - Service_Account_Delete_Mapper : 회원 탈퇴
    
    로 정의하였다.

---
+++ src/main/java/com/pos/system/mapper/Service_Account_Read_Mapper.java
@@ -0,0 +1,54 @@
+package com.pos.system.mapper;
+
+import com.pos.system.dto.Service_Account_DTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface Service_Account_Read_Mapper {
+
+    /**
+     * 모든 서비스 사용자를 조회하는 기능입니다.
+     * 삭제한 사용자는 조회되지 않습니다.
+     * @Param 없음
+     * @return List&lt;Service_Account_DTO&gt;
+     */
+    @Select(" SELECT * FROM SERVICE_ACCOUNT WHERE DELFLAG='N' ")
+    List<Service_Account_DTO> selectAll();
+
+
+
+    /**
+     * 서비스 사용자의 상세 정보를 조회합니다.
+     * @Param Service_Account_DTO
+     * @return Service_Account_DTO
+     */
+    @Select(" SELECT * FROM SERVICE_ACCOUNT WHERE SERVICE_ID = #{service_id} ")
+    Service_Account_DTO selectOne(Service_Account_DTO dto);
+
+
+    /**
+     * 회원 가입시 사용자가 조회할 아이디를 입력하여 아이디 중복검사를 하는 기능입니다
+     * 결과값은 중복되는 숫자로서 0이면 아이디 사용 가능, 1이상이면 아이디 중복을 의미합니다.
+     * @Param String
+     * @return int
+     */
+    @Select(" SELECT COUNT(*) FROM SERVICE_ACCOUNT WHERE SERVICE_ID = #{id} ")
+    int idDuplicate(String id);
+
+
+    /**
+     * 회원 가입 완료한 사용자가 로그인 하는 기능입니다.
+     * 사용자가 아이디와 비밀번호를 입력하여 리턴 Service_Account_DTO의 아이디가 null이면 로그인 실패, 아이디가 있으면 로그인 성공.
+     * @Param Service_Account_DTO
+     * @return Service_Account_DTO
+     */
+    @Select(" SELECT SERVICE_ID,SERVICE_TYPE,SERVICE_EMAIL FROM SERVICE_ACCOUNT WHERE SERVICE_ID=#{service_id} AND SERVICE_PW=#{service_pw} ")
+    Service_Account_DTO login(Service_Account_DTO dto);
+
+
+}
--- src/main/java/com/pos/system/mapper/Service_Account_Update_Mapper.java
+++ src/main/java/com/pos/system/mapper/Service_Account_Update_Mapper.java
@@ -1,15 +1,22 @@
 package com.pos.system.mapper;
 import com.pos.system.dto.Service_Account_DTO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public interface Service_Account_Modify_Mapper {
+public interface Service_Account_Update_Mapper {
 
-    //회원수정
+    /**
+     * 사용자의 상세정보인 이메일과 비밀번호를 수정하는 기능입니다.
+     * 사용자가 수정할 이메일와 비밀번호를 입력하여 리턴값이 0이면 수정 실패, 1이면 수정 성공
+     * @Param Service_Account_DTO
+     * @return int
+     */
     @Update(" UPDATE SERVICE_ACCOUNT SET SERVICE_EMAIL=#{service_email} , SERVICE_PW=#{service_pw} WHERE SERVICE_ID=#{service_id} ")
     int modify(Service_Account_DTO dto);
+
 
 }
