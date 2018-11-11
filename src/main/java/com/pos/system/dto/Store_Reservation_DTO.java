1be609a82bae8d546dca64d24bb13937ac100824
Brian Park <youstarkio@gmail.com>
2018-11-11 20:40:43 +0900

    20181111 예약테이블 작성 - 박성현
    - Store_Reservation DTO 작성
    - Store_Reservation_Create_Mapper 작성

--- src/main/java/com/pos/system/dto/Store_Reservation_DTO.java
+++ src/main/java/com/pos/system/dto/Store_Reservation_DTO.java
@@ -1,4 +1,97 @@
 package com.pos.system.dto;
 
 public class Store_Reservation_DTO {
+
+
+
+    private int reserv_seq;
+    private int store_seq;
+    private int table_seq;
+    private String user_id;
+    private String reserv_time;
+    private int reserv_people;
+    private String delflag;
+
+    public Store_Reservation_DTO() {
+    }
+
+    public Store_Reservation_DTO(int reserv_seq, int store_seq, int table_seq, String user_id, String reserv_time, int reserv_people, String delflag) {
+        this.reserv_seq = reserv_seq;
+        this.store_seq = store_seq;
+        this.table_seq = table_seq;
+        this.user_id = user_id;
+        this.reserv_time = reserv_time;
+        this.reserv_people = reserv_people;
+        this.delflag = delflag;
+    }
+
+    @Override
+    public String toString() {
+        return "Store_Reservation_DTO{" +
+                "reserv_seq=" + reserv_seq +
+                ", store_seq=" + store_seq +
+                ", table_seq=" + table_seq +
+                ", user_id='" + user_id + '\'' +
+                ", reserv_time='" + reserv_time + '\'' +
+                ", reserv_people=" + reserv_people +
+                ", delflag='" + delflag + '\'' +
+                '}';
+    }
+
+
+    public int getReserv_seq() {
+        return reserv_seq;
+    }
+
+    public void setReserv_seq(int reserv_seq) {
+        this.reserv_seq = reserv_seq;
+    }
+
+    public int getStore_seq() {
+        return store_seq;
+    }
+
+    public void setStore_seq(int store_seq) {
+        this.store_seq = store_seq;
+    }
+
+    public int getTable_seq() {
+        return table_seq;
+    }
+
+    public void setTable_seq(int table_seq) {
+        this.table_seq = table_seq;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getReserv_time() {
+        return reserv_time;
+    }
+
+    public void setReserv_time(String reserv_time) {
+        this.reserv_time = reserv_time;
+    }
+
+    public int getReserv_people() {
+        return reserv_people;
+    }
+
+    public void setReserv_people(int reserv_people) {
+        this.reserv_people = reserv_people;
+    }
+
+    public String getDelflag() {
+        return delflag;
+    }
+
+    public void setDelflag(String delflag) {
+        this.delflag = delflag;
+    }
 }
--- src/main/java/com/pos/system/mapper/Store_Reservation_Create_Mapper.java
+++ src/main/java/com/pos/system/mapper/Store_Reservation_Create_Mapper.java
@@ -1,9 +1,26 @@
 package com.pos.system.mapper;
 
+import com.pos.system.dto.Service_Store_DTO;
+import com.pos.system.dto.Store_Reservation_DTO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface Store_Reservation_Create_Mapper {
+
+    /**
+     * 새로운 예약을 하는 기능입니다.
+     * Service_Account에 존재하는 Service_id 가 있어야 생성할 수 있습니다.
+     * @param dto
+     * @return int 성공시 1, 실패시 0을 반환
+     */
+    @Insert(" INSERT STORE_RESERVATION(STORE_SEQ, TABLE_SEQ, USER_ID, RESERV_TIME, RESERV_PEOPLE, DELFLAG ) VALUES(#{store_Seq},#{table_Seq},#{user_id},CURRENT_TIMESTAMP,#{reserv_people},'N') ")
+    int createReserv(Store_Reservation_DTO dto);
+
+
+    @Insert(" INSERT INTO SERVICE_STORE(SERVICE_ID, STORE_NAME, STORE_DETAIL, STORE_TYPE, STORE_ADDRESS, STORE_TEL, STORE_HOUR, DELFLAG, REGDATE) VALUES(#{service_id}, #{store_name}, #{store_detail}, #{store_type}, #{store_address}, #{store_tel}, #{store_hour}, 'N', NOW()) ")
+    int createStore(Service_Store_DTO dto);
+
 }
