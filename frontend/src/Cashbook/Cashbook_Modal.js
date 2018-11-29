248a99d9f4511a6a62852826abbb194b85085e2f
Brian Park <youstarkio@gmail.com>
2018-11-29 09:15:05 +0900

    20181129 현금 출납부 리액트 구현 - 박성현 - index.js : jquery 작성 - 현금 출납부 리액트로 구현.

---
+++ frontend/src/Cashbook/Cashbook_Modal.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CashbookModal extends Component {
+
+    render() {
+        return (
+            <div>
+                <input type="text" placeholder="금액을 입력해주세요" className="form-control" />
+            </div>
+
+
+        );
+    }
+}
+
+export default CashbookModal;
